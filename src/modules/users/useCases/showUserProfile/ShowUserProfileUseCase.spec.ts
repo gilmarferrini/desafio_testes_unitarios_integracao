c58589473cbcd7d7a4834ed9de2e5cd5765f1d06
gilmarferrini <ferrinigilmar@gmail.com>
2021-10-26 19:44:53 -0300

    adicionado teste para retorno do perfil do usuario

---
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.spec.ts
@@ -0,0 +1,30 @@
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository"
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase"
+import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase"
+
+let inMemoryUsersRepository: InMemoryUsersRepository
+let createUserUseCase: CreateUserUseCase
+let showUserProfileUseCase: ShowUserProfileUseCase
+
+describe("Show User Profile", () => {
+  beforeEach(async () => {
+    inMemoryUsersRepository = new InMemoryUsersRepository()
+    showUserProfileUseCase = new ShowUserProfileUseCase(inMemoryUsersRepository)
+  })
+
+
+  it ("should be able to find user profile", async () => {
+    createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository)
+
+    const createdUser = await createUserUseCase.execute({
+      name: "valid name",
+      email: "valid_mail@mail.com",
+      password: "123456"
+    })
+
+    const response = await showUserProfileUseCase.execute(createdUser.id)
+
+    expect(response).toHaveProperty("id")
+    expect(response.name).toEqual("valid name")
+  })
+})
