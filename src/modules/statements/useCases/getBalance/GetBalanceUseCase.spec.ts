a42c04625f6471de3b5baf8e6688bf77a18ac079
gilmarferrini <ferrinigilmar@gmail.com>
2021-10-27 14:33:03 -0300

    adicionado teste para buscar o balance

---
+++ src/modules/statements/useCases/getBalance/GetBalanceUseCase.spec.ts
@@ -0,0 +1,47 @@
+import { AppError } from "../../../../shared/errors/AppError"
+import { InMemoryUsersRepository } from "../../../users/repositories/in-memory/InMemoryUsersRepository"
+import { InMemoryStatementsRepository } from "../../repositories/in-memory/InMemoryStatementsRepository"
+import { CreateStatementUseCase } from "../createStatement/CreateStatementUseCase"
+import { GetBalanceUseCase } from "./GetBalanceUseCase"
+
+let inMemoryUsersRepository: InMemoryUsersRepository
+let inMemoryStatementsRepository: InMemoryStatementsRepository
+let createStatementUseCase: CreateStatementUseCase
+let getBalanceUseCase: GetBalanceUseCase
+
+enum OperationType {
+  DEPOSIT = 'deposit',
+  WITHDRAW = 'withdraw',
+}
+
+
+describe("Get Balance", () => {
+
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository()
+    inMemoryStatementsRepository = new InMemoryStatementsRepository()
+    createStatementUseCase = new CreateStatementUseCase(inMemoryUsersRepository, inMemoryStatementsRepository)
+    getBalanceUseCase = new GetBalanceUseCase(inMemoryStatementsRepository, inMemoryUsersRepository)
+  })
+
+  it("should be able to get balance", async () => {
+    const user = await inMemoryUsersRepository.create({
+      name: "Test name",
+      email: "test@email.com",
+      password: "123456"
+    })
+
+    await createStatementUseCase.execute({ 
+      user_id: user.id, 
+      type: OperationType.DEPOSIT,
+      amount: 1000,
+      description: "deposit" 
+    })
+
+    const response = await getBalanceUseCase.execute({
+      user_id: user.id,
+    })
+
+    expect(response.statement.length).toEqual(1)
+  })
+})
