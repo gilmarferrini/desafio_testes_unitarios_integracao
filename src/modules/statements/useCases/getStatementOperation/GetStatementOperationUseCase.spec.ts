34abddf42cc6d51b9b927efddfa1552c3df99e61
gilmarferrini <ferrinigilmar@gmail.com>
2021-10-27 14:44:10 -0300

    adicionado teste para retorno de erro caso o statement nao existir

--- src/modules/statements/useCases/getStatementOperation/GetStatementOperationUseCase.spec.ts
+++ src/modules/statements/useCases/getStatementOperation/GetStatementOperationUseCase.spec.ts
@@ -1,3 +1,4 @@
+import { AppError } from "../../../../shared/errors/AppError"
 import { InMemoryUsersRepository } from "../../../users/repositories/in-memory/InMemoryUsersRepository"
 import { InMemoryStatementsRepository } from "../../repositories/in-memory/InMemoryStatementsRepository"
 import { GetStatementOperationUseCase } from "./GetStatementOperationUseCase"
@@ -39,6 +40,32 @@ describe("Get Statement", () => {
     })
 
     expect(response).toHaveProperty("id")
+  })
+
+  it("should not be able to get statement if user not exists", async () => {
+    expect(async () => {
+      await getStatementOperationUseCase.execute({
+        user_id: "12345",
+        statement_id: "213143"
+      })
+    }).rejects.toBeInstanceOf(AppError)
+  })
+
+  it("should not be able to get statement operation if statement not exists", async () => {
+    expect(async () => {
+      const user = await inMemoryUsersRepository.create({
+        name: "Test name",
+        email: "test@email.com",
+        password: "123456"
+      })
+  
+  
+      await getStatementOperationUseCase.execute({
+        user_id: user.id,
+        statement_id: "212314"
+      })
+    }).rejects.toBeInstanceOf(AppError)
+
   })
 
 })
