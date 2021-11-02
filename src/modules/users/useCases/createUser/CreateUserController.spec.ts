41869f6f16444c2005c09bcb97a0a91df48a9a97
gilmarferrini <ferrinigilmar@gmail.com>
2021-11-02 13:59:03 -0300

    adicionado teste para retorno de erro caso usuario ja exista

--- src/modules/users/useCases/createUser/CreateUserController.spec.ts
+++ src/modules/users/useCases/createUser/CreateUserController.spec.ts
@@ -2,6 +2,7 @@ import { app } from "../../../../app"
 import request from "supertest"
 import { Connection } from "typeorm"
 import { connectDatabase } from "../../../../database"
+import { AppError } from "../../../../shared/errors/AppError";
 
 let currentConnection: Connection;
 
@@ -24,9 +25,23 @@ describe("Create User Controller", () => {
       password: "123456"
     })
 
-    console.log(response.body)
-
     expect(response.statusCode).toBe(201)
+  })
+
+  it("should not be able to create a new user if user exists", async () => {
+    expect(async () => { 
+      await request(app).post("/api/v1/users").send({
+        name: "any name",
+        email: "anyemail@mail.com",
+        password: "123456"
+      })
+
+      await request(app).post("/api/v1/users").send({
+        name: "any name",
+        email: "anyemail@mail.com",
+        password: "123456"
+      })
+    }).rejects.toBeInstanceOf(AppError)
   })
 
 })
